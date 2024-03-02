0ca62549b83d93da66af63019a410b9866dfe5bf
Michal <m.stefanski98@gmail.com>
2024-03-02 18:41:57 +0100

    Adding set up

--- src/main/java/MieszkoEnterprices/MM/MmApplication.java
+++ src/main/java/MieszkoEnterprices/MM/MmApplication.java
@@ -1,7 +1,12 @@
 package MieszkoEnterprices.MM;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MmApplication {
@@ -10,4 +15,18 @@ public class MmApplication {
 		SpringApplication.run(MmApplication.class, args);
 	}
 
-}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let;s inspect the beans provided by SpringBoot: ");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for(String beanName: beanNames) {
+				System.out.println(beanName);
+			}
+			};
+		}
+	}
+
+
