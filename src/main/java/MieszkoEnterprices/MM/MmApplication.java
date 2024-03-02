381281844807bc15bbec02c56c3d7a636a97ce0b
Michal <m.stefanski98@gmail.com>
2024-03-02 17:58:11 +0100

    first commit

---
+++ src/main/java/MieszkoEnterprices/MM/MmApplication.java
@@ -0,0 +1,13 @@
+package MieszkoEnterprices.MM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MieszkoEnterprices/MM/MmApplicationTests.java
@@ -0,0 +1,13 @@
+package MieszkoEnterprices.MM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
