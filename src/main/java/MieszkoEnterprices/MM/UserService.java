5f7a63ce6e5aba60a174dc501d5ebe20619e06d8
Mateusz <mateusz@fakemail.com>
2024-06-19 20:50:37 +0200

    created user repo and user service

---
+++ src/main/java/MieszkoEnterprices/MM/UserService.java
@@ -0,0 +1,18 @@
+package MieszkoEnterprices.MM;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
